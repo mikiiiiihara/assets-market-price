fc7855cf3146f815207e972e1a3ef937adc07e7c
井原 樹/Miki Ihara <kimity0824@icloud.com>
2024-01-28 11:17:08 +0900

    changed async

--- schema/stock.py
+++ schema/stock.py
@@ -1,24 +1,24 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 from typing import List
+from concurrent.futures import ThreadPoolExecutor
+
+def fetch_single_stock_data(ticker: str):
+    one_year_ago = datetime.now() - timedelta(days=365)
+    stock = yf.Ticker(ticker)
+    current_price = stock.info.get("currentPrice") or stock.info.get("bid")
+    dividends = stock.dividends[stock.dividends.index >= one_year_ago.strftime("%Y-%m-%d")]
+
+    return {
+        "ticker": ticker,
+        "market_price": current_price,
+        "dividends": dividends.to_dict()
+    }
 
 def fetch_stock_data(tickers: List[str]):
-    stock_data = []
-    one_year_ago = datetime.now() - timedelta(days=365)
-    
-    for ticker in tickers:
-        stock = yf.Ticker(ticker)
-        current_price = stock.info.get("currentPrice") or stock.info.get("bid")
-        dividends = stock.dividends[stock.dividends.index >= one_year_ago.strftime("%Y-%m-%d")]
-
-        stock_data.append({
-            "ticker": ticker,
-            "market_price": current_price,
-            "dividends": dividends.to_dict()
-        })
-
-    return stock_data
-
+    with ThreadPoolExecutor(max_workers=len(tickers)) as executor:
+        results = list(executor.map(fetch_single_stock_data, tickers))
+    return results
 
 def resolve_stocks(*_, tickers: List[str]):
     data = fetch_stock_data(tickers)
